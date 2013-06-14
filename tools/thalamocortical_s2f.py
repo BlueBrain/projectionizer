d3bffe86f7aed1033db5d77e3f5f4a03d7281a64
Michael Reimann <michael.reimann@epfl.ch>
2013-06-14 10:42:27 +0200

    Added parameter estimation again, but in a different way.

--- tools/thalamocortical_s2f.py
+++ tools/thalamocortical_s2f.py
@@ -2,7 +2,7 @@ import h5py
 import numpy
 import pylab
 import scipy.stats
-import scipy.interpolate
+
 """"
 A very simple set of functions implementing a kind of 'Thalamocortical S2F'
 by only accepting connections with a certain min number of synapses.
@@ -10,32 +10,58 @@ by only accepting connections with a certain min number of synapses.
 """
 
 
-def thalamocortical_s2f(in_file, out_file, params):
+def thalamocortical_s2f(in_file, out_file, cutoff_var, target_mean = None, target_remove = None):
     in_h5 = h5py.File(in_file,'r')
-    out_h5 = h5py.File(out_file,'w')
-    real_params = (parameter_estimation(params[0],params[1]), params[1])
+    out_h5 = h5py.File(out_file,'w')    
+    if target_mean != None:
+        cutoff_mean = parameter_from_mean(in_h5, target_mean)
+    elif target_remove != None:
+        cutoff_mean = parameter_from_fraction_removed(in_h5, target_remove)
+    else:
+        raise RuntimeError("Must provide one of two parameters: target_mean or target_remove")
+    
     for k in in_h5.keys():
         data = numpy.array(in_h5[k])
-        post_lids = numpy.unique(data[:,0],return_inverse=True)[1]
-        unique_lids = numpy.unique(post_lids)
-        lid_counts = numpy.histogram(post_lids, bins=numpy.hstack((unique_lids,unique_lids[-1]+1)))
+        lid_counts, post_lids, unique_lids = count_syns_connection(data, return_lids=True, return_uniques=True)        
         accepted = numpy.ones_like(post_lids)
         for t_id,count in zip(unique_lids, lid_counts):
-            accepted[post_lids == t_id] = cutoff_func(count,params)
+            accepted[post_lids == t_id] = cutoff_func(count,(cutoff_mean, cutoff_var))
         
         if pylab.any(accepted):
             write_me = data[pylab.find(accepted),:]                
             out_h5.create_dataset(k, write_me.shape, ">f4", write_me)
-    
-def parameter_estimation(a, v):
-    norm = scipy.stats.norm.cdf
-    m = int(a)
-    r = 1 - (a - m)
-    tgt = m + 1
-    x = pylab.linspace(tgt+2*v,m-2*v,100)
-    f = norm(tgt,x,v)
-    func = scipy.interpolate.interp1d(f,x)
-    return func(r)    
+            
+def parameter_from_fraction_removed(in_h5, fraction):
+    lid_counts = []
+    for k in in_h5.keys():
+        data = numpy.array(in_h5[k])
+        lid_counts.extend(count_syns_connection(data))
+    count_counts = numpy.histogram(lid_counts,range(1,52))[0]
+    count_counts = [float((x+1)*y) for x,y in enumerate(count_counts)]
+    cumulative_count = numpy.hstack((0,numpy.cumsum(count_counts)/sum(count_counts)))
+    return numpy.interp(fraction,cumulative_count,range(1,len(cumulative_count)+1))-1
+
+def parameter_from_mean(in_h5,tgt_mn):
+    lid_counts = []
+    for k in in_h5.keys():
+        data = numpy.array(in_h5[k])
+        lid_counts.extend(count_syns_connection(data))
+    return tgt_mn - numpy.mean(lid_counts) + 0.5
+            
+def count_syns_connection(data, return_lids=False, return_uniques=False):
+    post_lids = numpy.unique(data[:,0],return_inverse=True)[1]
+    unique_lids = numpy.unique(post_lids)
+    return_me = numpy.histogram(post_lids, bins=numpy.hstack((unique_lids,unique_lids[-1]+1)))[0]
+    if return_lids:
+        if return_uniques:
+            return (return_me, post_lids, unique_lids)
+        else:
+            return (return_me, post_lids)
+    else:
+        if return_uniques:
+            return (return_me, unique_lids)
+        else:
+            return return_me
     
 def cutoff_func(x,params):
     norm = scipy.stats.norm.cdf
