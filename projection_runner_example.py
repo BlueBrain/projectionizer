e600e43a198552212fc874e585f17448e0a1e303
Michael Reimann <michael.reimann@epfl.ch>
2014-08-27 11:26:30 +0200

    Updated the default circuit path in the projection runner

--- projection_runner_example.py
+++ projection_runner_example.py
@@ -9,7 +9,7 @@ xml_file = "/home/ebmuller/src/bbp_svn_recipe/Projection_Recipes/Thalamocortical
 
 #cfg_file = "/bgscratch/bbp/circuits/23.07.12/SomatosensoryCxS1-v4.lowerCellDensity.r151/1x7_0/merged_circuit/CircuitConfig"
 
-cfg_file = "/bgscratch/bbp/circuits/23.07.12/SomatosensoryCxS1-v4.lowerCellDensity.r151/O1/merged_circuit/CircuitConfig"
+cfg_file = "/bgscratch/bbp/l5/release/2012.07.23/circuit/SomatosensoryCxS1-v4.lowerCellDensity.r151/O1/merged_circuit/CircuitConfig"
 
 composer = proj.ProjectionComposer(cfg_file, xml_file)
 if not os.path.exists("out"):
