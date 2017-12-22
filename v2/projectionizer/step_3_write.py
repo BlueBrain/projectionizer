ca0cc663af588b1babde856a4ff5d2fed6daab92
Mike Gevaert <michael.gevaert@epfl.ch>
2017-12-22 10:53:35 +0100

    separated step3_write
    Change-Id: If30d4951ca585feb04a5be659dab0e1da6100356

--- v2/projectionizer/step_3_write.py
+++ v2/projectionizer/step_3_write.py
@@ -5,7 +5,6 @@ import logging
 import os
 import traceback
 
-import h5py
 import numpy as np
 import pandas as pd
 from luigi import BoolParameter, FloatParameter, IntParameter
@@ -16,113 +15,9 @@ from projectionizer.luigi_utils import CommonParams, FeatherTask, JsonTask
 from projectionizer.step_1_assign import VirtualFibersNoOffset
 from projectionizer.step_2_prune import ChooseConnectionsToKeep, ReducePrune
 from projectionizer.utils import load, write_feather
+from projectionizer.write_nrn import write_synapses_summary, write_synapses
 
 L = logging.getLogger(__name__)
-
-
-class SynapseColumns(object):
-    '''columns index in nrn.h5 style file'''
-    SGID = 0
-    DELAY = 1
-
-    ISEC = 2
-    IPT = 3
-    OFFSET = 4
-
-    WEIGHT = 8  # G / CONDUCTANCE
-    U = 9
-    D = 10
-    F = 11
-    DTC = 12
-    SYNTYPE = 13
-    ASE = 17
-    NEURITE_TYPE = 18
-
-# from thalamocorticalProjectionRecipe_O1_TCs2f_7synsPerConn_os2p6_specific.xml
-# synaptic parameters are mainly derived from Amitai, 1997;
-# Castro-Alamancos & Connors 1997; Gil et al. 1997; Bannister et al. 2010 SR
-
-
-def create_synapse_data(synapses, synapse_params, efferent):
-    '''return numpy array for `synapses` with the correct parameters
-
-    Args:
-        synapses(pd.DataFrame): with the following columns:
-            sgid, y, Section.Id, Segment.Id, location
-    '''
-    synapse_count = len(synapses)
-    synapse_data = np.zeros((synapse_count, 19), dtype=np.float)
-
-    synapse_data[:, SynapseColumns.SGID] = synapses['tgid' if efferent else 'sgid'].values
-
-    # Note: this has to be > 0
-    # (https://bbpteam.epfl.ch/project/issues/browse/NSETM-256?focusedCommentId=56509)
-    # TODO: this needs to be a 'distance', for hex, 'y' makes sense, for 3D space, maybe less so
-    CONDUCTION_VELOCITY = 300.  # micron/ms, from original Projectionizer: InputMappers.py
-    synapse_data[:, SynapseColumns.DELAY] = synapses['y'].values / CONDUCTION_VELOCITY
-
-    synapse_data[:, SynapseColumns.ISEC] = synapses['section_id'].values
-    synapse_data[:, SynapseColumns.IPT] = synapses['segment_id'].values
-    offset = synapses['location'].values * np.random.ranf(synapse_count)
-    synapse_data[:, SynapseColumns.OFFSET] = offset
-
-    def gamma(param):
-        '''given `param`, look it up in SYNAPSE_PARAMS, return random pulls from gamma dist '''
-        return np.random.gamma(shape=synapse_params[param][0],
-                               scale=synapse_params[param][1],
-                               size=synapse_count)
-
-    synapse_data[:, SynapseColumns.WEIGHT] = gamma('gsyn')
-    synapse_data[:, SynapseColumns.U] = gamma('Use')
-    synapse_data[:, SynapseColumns.D] = gamma('D')
-    synapse_data[:, SynapseColumns.F] = gamma('F')
-    synapse_data[:, SynapseColumns.DTC] = gamma('DTC')
-    synapse_data[:, SynapseColumns.SYNTYPE] = synapse_params['id']
-
-    return synapse_data
-
-
-def write_synapses(path, itr, synapse_params, efferent=False):
-    '''write synapses to nrn.h5 style file
-
-    Args:
-        path(str): path to file to output
-        itr(tuple of (target GID, synapses), where synapses can be an iterable with
-        colums ['sgid', 'section_id', 'segment_id', 'location'] in order,
-        or a pandas DataFrame with those columns
-        If efferent==True, the column 'syn_ids' must also be present.
-    '''
-    with h5py.File(path, 'w') as h5:
-        info = h5.create_dataset('info', data=0)
-        info.attrs['version'] = 3
-        info.attrs['numberOfFiles'] = 1
-        for gid, synapses in itr:
-            synapse_data = create_synapse_data(synapses, synapse_params, efferent)
-            h5.create_dataset('a%d' % gid, data=synapse_data)
-
-            if efferent:
-                N = len(synapses["syn_ids"])
-                h5.create_dataset('a%d_afferentIndices' %
-                                  gid, data=synapses["syn_ids"].values.reshape((N, 1)))
-
-
-def write_synapses_summary(path, itr):
-    '''write synapses to nrn_summary.h5 style file
-
-    Args:
-        path(str): path to file to output
-        itr(tuple of (target GID, synapses), where connections is a
-        pandas DataFrame with columns ["connecting", "efferent", "afferent"]  as
-        per https://bbpteam.epfl.ch/project/spaces/display/BBPHPC/Synapses+-+nrn_summary.h5
-
-    '''
-    with h5py.File(path, 'w') as h5:
-        for dataset, connections in itr:
-            data = np.zeros((len(connections["connecting"].values), 3), dtype=np.int)
-            data[:, 0] = connections["connecting"].values
-            data[:, 1] = connections["efferent"].values
-            data[:, 2] = connections["afferent"].values
-            h5.create_dataset('a%d' % dataset, data=data)
 
 
 class WriteSummary(CommonParams):
@@ -135,6 +30,7 @@ class WriteSummary(CommonParams):
         # pylint thinks load() isn't returning a DataFrame
         # pylint: disable=maybe-no-member
         synapses = load(self.input().path)
+
         efferent = synapses.groupby(["sgid", "tgid"]).count()["segment_id"].reset_index()
         efferent.columns = ["dataset", "connecting", "efferent"]
         afferent = synapses.groupby(["tgid", "sgid"]).count()["segment_id"].reset_index()
@@ -143,6 +39,7 @@ class WriteSummary(CommonParams):
         summary.fillna(0, inplace=True)
         summary["efferent"] = summary["efferent"].astype(np.int32)
         summary["afferent"] = summary["afferent"].astype(np.int32)
+
         try:
             write_synapses_summary(path=self.output().path,
                                    itr=summary.groupby("dataset"))
---
+++ v2/projectionizer/tests/test_write_nrn.py
@@ -0,0 +1,74 @@
+import os
+from utils import setup_tempdir
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from projectionizer import write_nrn
+from numpy.testing import assert_allclose, assert_equal
+from nose.tools import ok_, raises
+
+
+SYNAPSE_PARAMS = {'id': 1,
+                  'gsyn': (3, 4),
+                  'Use': (5, 6),
+                  'D': (7, 8),
+                  'F': (9, 10),
+                  'DTC': (11, 12),
+                  'Ase': (13, 14)}
+
+
+def test_create_synapse_data():
+    np.random.seed(1337)
+
+    df = pd.DataFrame({'tgid': [1],
+                       'y': [0.33],
+                       'section_id': [1033],
+                       'location': [-3],
+                       'segment_id': [1033]})
+
+    assert_allclose(write_nrn.create_synapse_data(df, SYNAPSE_PARAMS, efferent=True),
+                    [[1.00000000e+00,   1.10000000e-03,   1.03300000e+03,   1.03300000e+03,
+                      -7.86074025e-01,   0.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                      7.75316318e+00,   1.92565485e+01,   2.51811971e+01,   9.06247588e+01,
+                      1.17854003e+02,   1.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                      0.00000000e+00,   0.00000000e+00,   0.00000000e+00]])
+
+
+def test_write_synapses():
+    np.random.seed(1337)
+    df = pd.DataFrame({'tgid': [1],
+                       'sgid': [2],
+                       'y': [0.33],
+                       'section_id': [1033],
+                       'location': [-3],
+                       'syn_ids': [12],
+                       'segment_id': [1033]})
+
+    with setup_tempdir('test_utils') as folder:
+        path = os.path.join(folder, 'projectionizer_test_write_synapses.h5')
+        write_nrn.write_synapses(path, df.groupby('sgid'),
+                                 SYNAPSE_PARAMS, efferent=False)
+
+        with h5py.File(path) as h5:
+            ok_('a2' in h5.keys())
+            assert_allclose(h5['a2'][...],
+                            [[2.00000000e+00,   1.10000000e-03,   1.03300000e+03,   1.03300000e+03,
+                              -7.86074025e-01,   0.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                              7.75316318e+00,   1.92565485e+01,   2.51811971e+01,   9.06247588e+01,
+                              1.17854003e+02,   1.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                              0.00000000e+00,   0.00000000e+00,   0.00000000e+00, ]])
+
+        path = os.path.join(folder, 'projectionizer_test_write_synapses_efferent.h5')
+        write_nrn.write_synapses(path, df.groupby('sgid'),
+                                 SYNAPSE_PARAMS, efferent=True)
+
+        with h5py.File(path) as h5:
+            ok_('a2' in h5.keys())
+            assert_allclose(h5['a2'][...],
+                            [[1.00000000e+00,   1.10000000e-03,   1.03300000e+03,   1.03300000e+03,
+                              -3.75173779e-01,   0.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                              5.80177543e+00,   5.08346474e+01,   4.77665138e+01,   1.46902748e+02,
+                              7.25006104e+01,   1.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                              0.00000000e+00,   0.00000000e+00,   0.00000000e+00]])
